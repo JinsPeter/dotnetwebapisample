b8a1d036e0069b540dfc1135374396b30d3983b8
jins <jins@qburst.com>
2017-06-12 18:17:13 +0530

    removed bin items

--- DotNetWebApiApp/DotNetWebApiApp/Controllers/FormController.cs
+++ DotNetWebApiApp/DotNetWebApiApp/Controllers/FormController.cs
@@ -1,7 +1,29 @@
-﻿namespace DotNetWebApiApp.Controllers
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using DotNetWebApiApp.Models;
+using App.DomainModels;
+namespace DotNetWebApiApp.Controllers
 {
+    [RoutePrefix("api/Form")]
     public class FormController : BaseController
     {
+        [Route("GetAll")]
+        [HttpGet]
+        public IHttpActionResult GetAllRecords()
+        {
+            var response = new ApiResponse<IEnumerable<FormModel>>();
+            response.Data = new List<FormModel>();
+            return Ok(response);
+        }
+
+        [Route("CreateNewUser")]
+        [HttpPost]
+        public IHttpActionResult CreateUser()
+        {
+            var response = new ApiResponse<int>();
+            response.Data = 5;
+            return Ok();
+        }
 
     }
 }
